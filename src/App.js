c6be76a77e55d8fe523445513c05ed95c7c5e637
python0220 <pythonking0220@gmail.com>
2024-03-28 21:13:24 +0530

    finalised

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import './App.css';
-import TodoInput from './components/TodoInp';
-import { useState } from 'react';
-import TodoList from './components/TodoList';
+import "./App.css";
+import TicTacToe from "./components/TicTacToe";
 
 function App() {
-
-  const [listTodo,setListTodo] = useState([]);
-  let addList = (inputText) => {
-    setListTodo([...listTodo,inputText]);
-  }
-
-  const deleteListItem = (key) => {
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key,1);
-    setListTodo([...newListTodo])
-  }
-
-  return (
-    <>
-      <div className='main-container'>
-        <div className='center-container'>
-            <TodoInput addList={addList}/>
-            <hr
-            style={{
-              backgroundColor: '#fff',
-              color: '#fff',
-              width: '100%',
-            }}
-            />
-            {listTodo.map((item,i)=> {
-              return  (
-                <TodoList task={item} key={i}
-                index = {i}
-                deleteItem = {deleteListItem}
-                />
-              )
-            })}
-            {/* <div className='TodoListClass' task={}> */}
-            {/* <TodoList/> */}
-
-            {/* </div> */}
-        </div>
-      </div>
-    </>
-  );
+  return <TicTacToe />;
 }
 
 export default App;
